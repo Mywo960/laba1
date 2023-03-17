de3047cd00e43e507a4c894ca4c04c390fb0f6fa
Kirill <mywo960@gmail.com>
2023-03-17 13:48:10 +0600

    1

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
 
+def parse():
+    header = {
+        #Сюда помещаем наш user-agent
+        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.3.949 Yowser/2.5 Safari/537.36'
+    }
+    url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
+    spisok = {}
+    resp = requests.get(url, headers=header).text
 
-header = {
-    #Сюда помещаем наш user-agent
-    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.3.949 Yowser/2.5 Safari/537.36'
-}
-url = 'https://www.imdb.com/chart/top/?ref_=nv_mv_250'
-spisok = {}
-resp = requests.get(url, headers=header).text
-soup = BeautifulSoup(resp, "html.parser")
-block3 = soup.findAll('tr')
-block1 = soup.findAll('td', class_='titleColumn')
-block2 = soup.findAll('td', class_='ratingColumn imdbRating')
+    soup = BeautifulSoup(resp, "html.parser")
+    block1 = soup.findAll('td', class_='titleColumn')
+    block2 = soup.findAll('td', class_='ratingColumn imdbRating')
 
-rating = ''
-name = ''
-list = []
-count = 0
-for data in block1:
+    rating = ''
+    name = ''
+    list = []
+    count = 0
+    for data in block1:
 
-     if data.find('a'):
-        name = data.text
-        name = name.replace('\n', '')
-        name = name.replace('  ', '')
-        list.append(name)
+         if data.find('a'):
+            name = data.text
+            name = name.replace('\n', '')
+            name = name.replace('  ', '')
+            list.append(name)
 
 
 
-for data1 in block2:
+    for data1 in block2:
 
-    if data1.find('strong'):
-        rating = data1.text
-        rating = rating.replace('\n', '')
-        rating = rating.replace('  ', '')
-        spisok[list[count]] = rating
-        if count == 250:
-            break
-        count += 1
+        if data1.find('strong'):
+            rating = data1.text
+            rating = rating.replace('\n', '')
+            rating = rating.replace('  ', '')
+            spisok[list[count]] = rating
+            if count == 250:
+                break
+            count += 1
 
-print(spisok)
+    print(spisok)
+
+if __name__ == '__main__':
+    parse()
+
+
 
